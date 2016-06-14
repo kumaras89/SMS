fb631d1c5a9bb087456c77c09919b1d4e7a9504b
snagasubramaniyam <snagasubramaniyam@bridge2solutions.com>
2016-06-14 15:32:33 +0530

    New UI - version 2

---
+++ sms-webapp/src/main/webapp/public/home/dashboard.controller.js
@@ -0,0 +1,39 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('DashBoardCtrl', ['$scope', 'CrudService', 'FlashService', '$location', 'AdminService', '$cookieStore',
+            function ($scope, CrudService, FlashService, $location, AdminService, $cookieStore) {
+
+                $scope.loggedInBranchCode = $cookieStore.get('globals').currentUser.otherDetails.branch;
+
+                $scope.getBranchDesc = function (branchCode) {
+                    return AdminService.getBranchDesc(branchCode);
+                };
+
+                $scope.getCourseDesc = function (courseCode) {
+                    return AdminService.getCourseDesc(courseCode);
+                };
+
+                $scope.getSchemeDesc = function (schemeCode) {
+                    return AdminService.getSchemeDesc(schemeCode);
+                };
+
+                $scope.loadStudents = function () {
+                    CrudService.studentService.GetAll().then(function (res) {
+                        if (res.message) {
+                            $scope.students = []
+                            FlashService.Error(res.message)
+                        } else {
+                            $scope.students = res
+                        }
+                    }, function () {
+                        $scope.students = []
+                    })
+                }
+
+                $scope.loadStudents()
+            }]);
+
+})();
