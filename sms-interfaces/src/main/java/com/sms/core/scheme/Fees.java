a3cebd330a8784371578164b295abda8605ef063
Krishna <krishna.kb@gmail.com>
2016-06-20 12:40:40 +0530

    Added validations

--- sms-interfaces/src/main/java/com/sms/core/scheme/Fees.java
+++ sms-interfaces/src/main/java/com/sms/core/scheme/Fees.java
@@ -7,11 +7,13 @@ import javax.persistence.Column;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.MappedSuperclass;
+import javax.validation.Valid;
 import java.math.BigDecimal;
 
 @MappedSuperclass
 public abstract class Fees extends BaseModel {
 
+    @Valid
     @ManyToOne
     @JoinColumn(name = "FC_FEES_PARTICULAR_ID")
     private FeesParticular feesParticular;
